035d8589e5b62dc9437d54604af666e2180fd521
kx1t <kx1t@amsat.org>
2022-02-01 10:29:00 +0000

    updated build script

--- buildnow.sh
+++ buildnow.sh
@@ -14,13 +14,13 @@ git pull
 # make the build certs root_certs folder:
 # Note that this is normally done as part of the github actions - we don't have those here, so we need to do it ourselves before building:
 #ls -la /etc/ssl/certs/
-mkdir -p ./root_certs/etc/ssl/certs
-mkdir -p ./root_certs/usr/share/ca-certificates/mozilla
-cp --no-dereference /etc/ssl/certs/*.crt ./root_certs/etc/ssl/certs
-cp --no-dereference /etc/ssl/certs/*.pem ./root_certs/etc/ssl/certs
-cp --no-dereference /usr/share/ca-certificates/mozilla/*.crt ./root_certs/usr/share/ca-certificates/mozilla
+#mkdir -p ./root_certs/etc/ssl/certs
+#mkdir -p ./root_certs/usr/share/ca-certificates/mozilla
+#cp --no-dereference /etc/ssl/certs/*.crt ./root_certs/etc/ssl/certs
+#cp --no-dereference /etc/ssl/certs/*.pem ./root_certs/etc/ssl/certs
+#cp --no-dereference /usr/share/ca-certificates/mozilla/*.crt ./root_certs/usr/share/ca-certificates/mozilla
 
 DOCKER_BUILDKIT=1 docker buildx build --progress=plain --compress --push $2 --platform linux/armhf,linux/arm64 --tag kx1t/webproxy:$TAG .
 
-rm -rf ./root_certs
+#rm -rf ./root_certs
 popd
