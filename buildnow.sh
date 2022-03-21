ef04504b22baeb7b78efd36495f2ffeaab7fad33
kx1t <kx1t@amsat.org>
2022-03-21 09:14:51 +0000

    updated build script for pushing to ghcr and docker

--- buildnow.sh
+++ buildnow.sh
@@ -6,19 +6,22 @@ set -x
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
 [[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64,linux/i386"
 
-BASETARGET=ghcr.io/sdr-enthusiasts
-# BASETARGET=kx1t
+BASETARGET1=ghcr.io/sdr-enthusiasts
+BASETARGET2=kx1t
 
-IMAGE="$BASETARGET/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+IMAGE1="$BASETARGET1/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
 
-[[ "$IMAGE" == "$BASETARGET/reversewebproxy:$TAG" ]] && IMAGE="$BASETARGET/webproxy:$TAG" || true
+[[ "$IMAGE1" == "$BASETARGET1/reversewebproxy:$TAG" ]] && IMAGE="$BASETARGET1/webproxy:$TAG" || true
+[[ "$IMAGE2" == "$BASETARGET2/reversewebproxy:$TAG" ]] && IMAGE="$BASETARGET2/webproxy:$TAG" || true
 
-echo "press enter to start building $IMAGE from $BRANCH"
+echo "press enter to start building $IMAGE1 and $IMAGE2 from $BRANCH"
 read
 
 starttime="$(date +%s)"
 # rebuild the container
 git checkout $BRANCH || exit 2
 git pull -a
-docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE .
+docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE1 .
+docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE2 .
 echo "Total build time: $(( $(date +%s) - starttime )) seconds"
